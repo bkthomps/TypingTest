ea464e02cfe01e0c355bfaa52e4653e8cbbfb86e
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-02-01 21:31:57 -0500

    Version 1.1.0

--- src/typingtest/package-info.java
+++ src/typingtest/package-info.java
@@ -5,7 +5,7 @@
  * <br>
  * To start the typing test start typing the second line of words.&nbsp;They will then disappear until no words are 
  * left, and at that point, the second line of words will be the first line of words.&nbsp;The words are generated from 
- * a list of the approximately ten thousand most common words.&nbsp;Once done the test, your words per minute (WPM) and 
+ * a list of the approximately nine thousand most common words.&nbsp;Once done the test, your words per minute (WPM) and 
  * characters per minute (CPM) will be displayed.&nbsp;The high score is also kept even through different instances of 
  * play.
  */
