cbc54aace8fa081f7f8afca225b2addabea3b7ca
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-02-07 22:26:42 -0500

    Version 1.2.0

--- src/typingtest/package-info.java
+++ src/typingtest/package-info.java
@@ -1,12 +1,12 @@
 /**
  * Bailey Thompson <br>
- * Typing Test (1.1.0) <br>
- * 1 February 2016 <br>
+ * Typing Test (1.2.0) <br>
+ * 7 February 2017 <br>
  * <br>
- * To start the typing test start typing the second line of words.&nbsp;They will then disappear until no words are 
- * left, and at that point, the second line of words will be the first line of words.&nbsp;The words are generated from 
- * a list of the approximately nine thousand most common words.&nbsp;Once done the test, your words per minute (WPM) and 
- * characters per minute (CPM) will be displayed.&nbsp;The high score is also kept even through different instances of 
+ * To start the typing test start typing the second line of words.&nbsp;They will then disappear until no words are
+ * left, and at that point, the second line of words will be the first line of words.&nbsp;The words are generated from
+ * a list of the approximately ten thousand most common words.&nbsp;Once done the test, your words per minute (WPM) and
+ * characters per minute (CPM) will be displayed.&nbsp;The high score is also kept even through different instances of
  * play.
  */
 package typingtest;
