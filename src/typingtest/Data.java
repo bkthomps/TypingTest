85c5d6d6e0db9dca32f545024394909bb613fcd4
Bailey Thompson <bailey.kyle.thompson@gmail.com>
2017-02-07 22:24:30 -0500

    Refactor

--- src/typingtest/Data.java
+++ src/typingtest/Data.java
@@ -3,17 +3,17 @@ package typingtest;
 /**
  * Special data type.
  */
-public class Data {
-    
-    static final int CHAR_TO_WORD = 5;
+class Data {
+
+    private static final int CHAR_TO_WORD = 5;
     static final int WORDS_PER_LINE = 5;
     static final int TIME_IN_SECONDS_PER_ROUND = 60;
 
     static int rightWords;
     static int wordIndex;
-    
-    static String[] topWords = new String[WORDS_PER_LINE];
-    static String[] botWords = new String[WORDS_PER_LINE];
+
+    static final String[] topWords = new String[WORDS_PER_LINE];
+    static final String[] botWords = new String[WORDS_PER_LINE];
     static String currentWord = "";
     static int time;
     static int lastCPM, lastWPM;
@@ -22,11 +22,11 @@ public class Data {
     private Data() {
         //not used
     }
-    
+
     public static void resetValues() {
         lastWPM = rightWords * TIME_IN_SECONDS_PER_ROUND / 60;
         lastCPM = lastWPM * CHAR_TO_WORD;
-        
+
         rightWords = 0;
         wordIndex = 0;
 
